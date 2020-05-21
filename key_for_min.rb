399fb48218aa7bbfbd69cfc67c5c38527243cbfb
Jodi Croft <jodipruett@gmail.com>
2020-05-21 17:32:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_key = name_hash[key][0]
-  lowest_value = name_hash[value][0]
-
+  first_key = name_hash[0][0]
 end
 
 
