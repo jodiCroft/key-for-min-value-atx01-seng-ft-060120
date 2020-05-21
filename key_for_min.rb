79c0d15e8e940ad03f20b4b34d3522828b850d66
Jodi Croft <jodipruett@gmail.com>
2020-05-21 17:43:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,7 +14,3 @@ def key_for_min_value(name_hash)
 end
 
 
-
-#take the first value and store it, then go over each one and replace what is in store if its less than whats
-#in there. Do this will all the values and then when all done, return the key that goes with the value thats
-#in there. Or maybe store both the key and value and then return only the key for the smallest value
