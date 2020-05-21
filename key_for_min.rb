36dbbd251353a684f37a03466a99c6cef4c9801a
Jodi Croft <jodipruett@gmail.com>
2020-05-21 16:59:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_pair = {name_hash[0]}
+  name_hash.each do |key, value|
 
+  end
 end
+
+
+
+#take the first value and store it, then go over each one and replace what is in store if its less than whats
+#in there. Do this will all the values and then when all done, return the key that goes with the value thats
+#in there. Or maybe store both the key and value and then return only the key for the smallest value
