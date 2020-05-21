51530771266d1ee6d6b5785cd99553d7ada4e042
Jodi Croft <jodipruett@gmail.com>
2020-05-21 17:27:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_pair = {}
+  lowest_key = []
+  lowest_value = []
+  name_hash.each do |key, value|
+    if lowest_value.empty? && value < lowest_value
+      lowest_value = value
+  end
 end
 
 
